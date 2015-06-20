cb196c3a473a38edd04876f936e7b3227467d8e2
Earl Hinrichs <earl@spyke.com>
2015-06-20 09:39:53 -0600

    Assignment2 - Cache matrix invers

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Goal: Create an enhanced matix object "CacheMatrix" which caches its inverse.
+## The object created is a list of functions
+##      get(): returns the matrix as a matrix class object
+##      invert(): returns the inverse of the matrix.
 
-## Write a short comment describing this function
+
+## makeCacheMatrix creates an instance of out special matrix from an ordinary matrix
+## Example usage: (x is an invertible matrix)
+##  cacheMatrix <- makeCacheMatrix(x)   ##Create an instance of the special matrix from x
+##  copyOfX <- cacheMatrix$get()        ##Get a copy of the original matrix
+##  inverse <- cacheMatrix$invert()     ##Get the inverse, first time is computed.
+##  inverse <- cacheMatrix$invert()     ##Get the inverse, second time fetches from cache
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ##Keep track of the matrix and it's inverse.
+        ##Use lazy evaluation for the inverse. Only compute if necessary
+    thisMatrix <- x
+    inverse <- NULL
+        ## return a copy of the original matrix
+    get <- function(y) thisMatrix
+        ## return the inverse of the matrix.
+        ## compute if necessary, otherwise use cache copy
+    invert <- function() {
+        if( is.null(inverse) ) {
+            inverse <<- solve(thisMatrix)
+        }
+        inverse
+    }
+        ##return a list with the get and invert functions
+    list(get=get, invert=invert)
 }
 
 
-## Write a short comment describing this function
+## cachesolve returns the inverse of a cachematrix that was created
+## with makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    x$invert()
 }
+
